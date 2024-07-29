e1699454940038f3f3bb491cd78ba7cce9d8e75e
Siddhi Behere <122604852+SiddhiBehere@users.noreply.github.com>
2024-07-29 19:19:54 +0530

    Create vibe_data.kt

---
+++ app/src/main/java/com/example/fashionmatch/vibe_data.kt
@@ -0,0 +1,16 @@
+package com.example.fashionmatch
+
+data class vibe_data(
+    val name: String = "",
+    val vibe_img: String = "",
+    val vibe_img1: String = "",
+    val vibe_img2: String = "",
+    val vibe_img3: String = "",
+    val vibe_img4: String = "",
+    val vibe_tagline: String = "",
+    val vibe_text1: String = "",
+    val vibe_text2: String = "",
+    val vibe_text3: String = "",
+    val vibe_text4: String = "",
+    val vibeq: String = ""
+)
